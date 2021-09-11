aeb5aaf7f9c09397f0ee032fd23f816af962fc17
Tadahisa MOTOOKA <motooka@jotojoho.co.jp>
2021-09-11 12:19:41 +0900

    initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # image-splitter
 A CLI tool which splits an image into Red-Green-Blue layers
+
+## How to Use
+`image-splitter /file/path/to/source-image.png`
+
+The command will create the following files in the same directory
+
+- `source-image-red.png`
+- `source-image-green.png`
+- `source-image-blue.png`
+
+### Options
+TODO
+
+## Development Environment
+Go 1.17 is required
+
+## Development Operations
+- `go build image-splitter.go` to build executable, named `image-splitter`
+- `go run image-splitter.go` to execute
---
+++ image-splitter.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		println("give an argument of source file path")
+		os.Exit(1)
+	}
+	source := os.Args[1]
+	fmt.Println("splitting the file : " + source)
+	err := split(source)
+	if err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+}
+
+type layer struct {
+	Name      string
+	Converter func(color.Color) color.Gray16
+}
+
+func split(source string) error {
+	f, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return err
+	}
+
+	// split
+	layers := []layer{
+		{"red", rgbaRedToGray},
+		{"green", rgbaGreenToGray},
+		{"blue", rgbaBlueToGray},
+		{"alpha", rgbaAlphaToGray},
+	}
+	for _, v := range layers {
+		outFilePath := outputPath(source, v.Name)
+		fmt.Println("writing the file : " + outFilePath)
+		outFile, err := os.Create(outFilePath)
+		if err != nil {
+			return err
+		}
+		outImage := extractLayer(img, v.Converter)
+		err = png.Encode(outFile, outImage)
+		outFile.Close()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func outputPath(source string, version string) string {
+	// e.g. "~/hoge/test.png" => "~/hoge/test-red.png"
+	dir := filepath.Dir(source)
+	base := filepath.Base(source)
+	ext := filepath.Ext(source)
+	outputFile := base + "-" + version + ext
+	outputPath := filepath.Join(dir, outputFile)
+	return outputPath
+}
+
+func extractLayer(img image.Image, convFunc func(color.Color) color.Gray16) *image.Gray16 {
+	bounds := img.Bounds()
+	outImage := image.NewGray16(bounds)
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			outImage.Set(x, y, convFunc(img.At(x, y)))
+		}
+	}
+	return outImage
+}
+
+func rgbaRedToGray(rgba color.Color) color.Gray16 {
+	r, _, _, _ := rgba.RGBA() // uint32
+	var gray color.Gray16
+	gray.Y = uint16(r)
+	return gray
+}
+func rgbaGreenToGray(rgba color.Color) color.Gray16 {
+	_, g, _, _ := rgba.RGBA() // uint32
+	var gray color.Gray16
+	gray.Y = uint16(g)
+	return gray
+}
+func rgbaBlueToGray(rgba color.Color) color.Gray16 {
+	_, _, b, _ := rgba.RGBA() // uint32
+	var gray color.Gray16
+	gray.Y = uint16(b)
+	return gray
+}
+func rgbaAlphaToGray(rgba color.Color) color.Gray16 {
+	_, _, _, a := rgba.RGBA() // uint32
+	var gray color.Gray16
+	gray.Y = uint16(a)
+	return gray
+}
